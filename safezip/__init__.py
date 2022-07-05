c5f9a75fb755a5bade480bbeddf10a885ed4b9a6
Thomas Bouve <thomas.bouve@be.ibm.com>
2022-07-05 14:12:07 +0200

    Add context manager support.

--- safezip/__init__.py
+++ safezip/__init__.py
@@ -1,6 +1,8 @@
 """Top-level package for SafeZip."""
-from typing import Protocol
-from io import BytesIO
+from typing import Protocol, Union
+from io import BytesIO, IOBase
+from pathlib import PurePath
+from os import PathLike
 import pyzipper
 
 
@@ -18,33 +20,42 @@ class ResponseLike(Protocol):
 class SafeZip:
     """A password protected in-memory ZIP file."""
 
-    def __init__(self, password: str = "infected"):
+    def __init__(
+            self,
+            file: Union[PurePath, PathLike, str, IOBase],  # types supported by `pyzipper`
+            password: str = "infected"):
         """Create a new SafeZip object."""
-        self.password = bytes(password, 'utf-8')
-        self.mem_zip = BytesIO()
+        self.zf = pyzipper.AESZipFile(
+            file,
+            mode="w",
+            compression=pyzipper.ZIP_LZMA,
+            encryption=pyzipper.WZ_AES)
+        self.zf.setpassword(bytes(password, 'utf-8'))
 
-    def add_from_bytes(self, data: bytes, filename: str) -> None:
+    def add_from_bytes(self, filename: str, data: bytes) -> None:
         """Add file data from bytes."""
-        with pyzipper.AESZipFile(
-                self.mem_zip,
-                mode="a",
-                compression=pyzipper.ZIP_LZMA,
-                encryption=pyzipper.WZ_AES) as zf:
-            zf.setpassword(self.password)
-            zf.writestr(filename, data)
+        self.zf.writestr(filename, data)
 
-    def add_from_iostream(self, data: BytesIO, filename: str) -> None:
+    def add_from_iostream(self, filename: str, data: BytesIO) -> None:
         """Add file data from an IO Stream."""
         data.seek(0)
-        self.add_from_bytes(data.read(), filename)
+        self.add_from_bytes(filename, data.read())
 
-    def add_from_http_response(self, data: ResponseLike, filename: str) -> None:
+    def add_from_http_response(self, filename: str, data: ResponseLike) -> None:
         """Add file data from a Requests or Httpx Response object."""
         if data.status_code >= 200 and data.status_code < 300:
-            self.add_from_bytes(data.content, filename)
+            self.add_from_bytes(filename, data.content)
         else:
             raise BytesWarning("HTTP {}: unable to retrieve data. Skipping bytes.".format(data.status_code))
 
-    def get_bytes(self) -> bytes:
-        """Return the ZIP file as bytes."""
-        return self.mem_zip.getvalue()
+    def close(self):
+        """Close """
+        self.zf.close()
+
+    def __enter__(self):
+        """Context manager function."""
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        """Context manager function."""
+        self.close()
--- sample_usage.py
+++ sample_usage.py
@@ -1,19 +1,25 @@
 """This file demonstrates the usage of In-Memory Zipper."""
 from safezip import SafeZip
 from pathlib import Path
+from io import BytesIO
 import requests
 
-# Create the SafeZip object with a 'hello' password
-sz = SafeZip(password='hello')
+# Longer approach without a context manager
+sz = SafeZip(Path("encrypted1.zip"), password='hello')
+sz.add_from_bytes("readme-md.txt", Path("README.md").read_bytes())
+sz.add_from_http_response("example-com.html", requests.get('http://example.com'))
+sz.close()
 
-# Add from bytes
-input_file = Path("README.md")
-input_file_name = "readme-md.txt"
-sz.add_from_bytes(input_file.read_bytes(), input_file_name)
+# Context manager with data saved straight to an encrypted ZIP file
+with SafeZip(Path("encrypted2.zip"), password='hello') as sz:
+    sz.add_from_bytes("readme-md.txt", Path("README.md").read_bytes())
+    sz.add_from_http_response("example-com.html", requests.get('http://example.com'))
 
-# Add from HTTP response
-sz.add_from_http_response(requests.get('http://example.com'), "example-com.html")
+# Context manager with data saved to an encrypted ZIP file via an in-memory buffer
+buff = BytesIO()
 
-# Write the ZIP file
-zip_output_file = Path("encrypted.zip")
-zip_output_file.write_bytes(sz.get_bytes())
+with SafeZip(buff, password='hello') as sz:
+    sz.add_from_bytes("readme-md.txt", Path("README.md").read_bytes())
+    sz.add_from_http_response("example-com.html", requests.get('http://example.com'))
+
+Path("encrypted3.zip").write_bytes(buff.getvalue())
